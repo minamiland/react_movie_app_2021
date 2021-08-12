20b732310c555debde3fd2d39e2a907409208033
minamiland <minamiland@gmail.com>
2021-08-13 08:57:46 +0900

    함수학습

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,17 +1,54 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h2>I like { fav }</h2>;
-}
-
-function App() {
+function Food({ name, picture }) {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Food fav="kimchi" />
-      <Food fav="banana" />
-      <Food fav="apple" />
-      <Food fav="strawberry" />
+      <h2>I like { name }</h2>
+      <img src={ picture } alt="" />
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    name: 'Kimchi',
+    image: 'http://via.placeholder.com/300x100/000FF/808080'
+  },
+  {
+    name: 'Coffee',
+    image: 'http://via.placeholder.com/300x100/FF0000/FFFFFF'
+  },
+  {
+    name: 'Banana',
+    image: 'http://via.placeholder.com/300x100/FFFF00/000000'
+  },
+  {
+    name: 'Apple',
+    image: 'http://via.placeholder.com/300x100/CCCC00/EEEEEE'
+  },
+];
+
+// ES5 함수 사용
+// function renderFood(dish) {
+//   return <Food name={dish.name} picture={dish.image} />;
+// }
+
+// ES6 함수 사용
+const renderFood = dish => <Food name={dish.name} picture={dish.image} />;
+
+function App() {
+  console.log(foodLike.map(renderFood));
+  return (
+    <div>
+      { 
+        // 외부함수로 분리
+        // foodLike.map(dish => (
+        //   <Food name={dish.name} picture={dish.image} />
+        // ))
+
+        // 외부함수로 관리 
+        foodLike.map(renderFood)
+      }
     </div>
   );
 }
