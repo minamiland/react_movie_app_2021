fa79dcde1fd87f2eed6f2249784e6cbfdbf3fe93
minamiland <minamiland@gmail.com>
2021-08-13 06:59:32 +0900

    update

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+function Food({ fav }) {
+  return <h2>I like { fav }</h2>;
+}
+
 function App() {
   return (
-    <div className="App">Hello!</div>
+    <div>
+      <h1>Hello!</h1>
+      <Food fav="kimchi" />
+      <Food fav="banana" />
+      <Food fav="apple" />
+      <Food fav="strawberry" />
+    </div>
   );
 }
 
