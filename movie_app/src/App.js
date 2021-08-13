dd0e23e12a0aa47610b7093af57ad08c5ecc40a6
minamiland <minamiland@gmail.com>
2021-08-13 17:03:52 +0900

    [Added] axios 플러그인 설치

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movie: []
   };
 
   componentDidMount() {
+    // 영화 데이터 로딩
+    // https://yts-proxy.vercel.app/list_movies.json
     setTimeout(() => {
       this.setState({ isLoading: false });
-      console.log('componentDidMount');
+      console.log('componentDidMount: 영화 데이터 로딩');
     }, 6000)
   }
 
