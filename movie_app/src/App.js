940be3407a3a25912aa650a610fac9f299cb5efa
minamiland <minamiland@gmail.com>
2021-08-13 11:00:55 +0900

    [Added] prop-types 플러그인 설치 및 적용

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// https://www.npmjs.com/package/prop-types
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
@@ -13,22 +16,26 @@ const foodLike = [
   {
     id: 1,
     name: 'Kimchi',
-    image: 'http://via.placeholder.com/300x100/000FF/808080'
+    image: 'http://via.placeholder.com/300x100/000FF/808080',
+    rating: 3
   },
   {
     id: 2,
     name: 'Coffee',
-    image: 'http://via.placeholder.com/300x100/FF0000/FFFFFF'
+    image: 'http://via.placeholder.com/300x100/FF0000/FFFFFF',
+    rating: 4.5
   },
   {
     id: 3,
     name: 'Banana',
-    image: 'http://via.placeholder.com/300x100/FFFF00/000000'
+    image: 'http://via.placeholder.com/300x100/FFFF00/000000',
+    rating: 2.5
   },
   {
     id: 4,
     name: 'Apple',
-    image: 'http://via.placeholder.com/300x100/CCCC00/EEEEEE'
+    image: 'http://via.placeholder.com/300x100/CCCC00/EEEEEE',
+    rating: 5
   },
 ];
 
@@ -47,7 +54,7 @@ function App() {
       { 
         // 외부함수로 분리
         foodLike.map(dish => (
-          <Food key={dish.id} name={dish.name} picture={dish.image} />
+          <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
         ))
 
         // 외부함수로 관리 
@@ -56,5 +63,11 @@ function App() {
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 export default App;
