b438ee33ba0fe7aaca2469d32fac1e5923709b5f
minamiland <minamiland@gmail.com>
2021-08-13 11:25:16 +0900

    class에 state 추가 및 적용 방법

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,13 +4,29 @@ import React from 'react';
 class App extends React.Component {
   // App 클래스에 React.Component를 상속시킨다.
   // React.Component 클래스의 기능을 추가한 App 클래스
+  state = {
+      count: 0
+  };
+
+  add = () => {
+    console.log('add');
+  };
+
+  minus = () => {
+    console.log('minus');
+  };
   
   // 클래스에서는 return 문을 사용할 수 없다.
   // 함수형 컴포넌트는 return 문이 JRX를 반환
   // 클래스형 컴포넌트는 render() 함수가 JSX를 반환
   render() {
-
-    return <h1>I'm a class Component</h1>;
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
 }
 
