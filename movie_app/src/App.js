2bcae6f269767de60719d2270f0b0ea53920205d
minamiland <minamiland@gmail.com>
2021-08-14 13:27:10 +0900

    Movie 컴포넌트 HTML/CSS 추가

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -44,21 +44,27 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
     // return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
     return (
-      <div>
-        { isLoading ? 'Loading...' : movies.map((movie) => {
-          console.log(movie);
-          return (
-            <Movie 
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />
-          );
-        })}
-      </div>
+      <section className="container">
+        { isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              // console.log(movie);
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie({id, title, summary, poster}) {
-  return <h4>{title}</h4>;
+function Movie({id, title, year, summary, poster}) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <p className="movie_summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = { 
   id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired
